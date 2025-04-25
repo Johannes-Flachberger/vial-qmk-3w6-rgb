f57486dfffcb735788083227bb207191d0725be7
Johannes-Flachberger <jo.flachberger@gmail.com>
2025-04-25 16:12:59 +0200

    chore: remove old keymap

--- keyboards/keebart/3w6_rgb/keymaps/arctic_alt/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
-
-#define RGB_MATRIX_SLEEP     // turn off effects when suspended
-#undef RGB_MATRIX_DEFAULT_MODE
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
